eb0e36fe26465a9196efbecc2216d0f39b802154
OWAIS KHAN <khan.owais0555@gmail.com>
2025-04-19 23:53:12 +0530

    DB created in system

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,12 @@
 import { Stack } from "expo-router";
+import { useFonts } from 'expo-font';
 
 export default function RootLayout() {
+  
+  const [loaded, error] = useFonts({
+    'Outfit': require('../assets/fonts/Outfit-Regular.ttf'),
+    'Outfit-Bold': require('../assets/fonts/Outfit-Bold.ttf'),
+
+  });
   return <Stack />;
 }
